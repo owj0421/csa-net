f4651bb6a5870a3ef55a85887a90a55bab059174
big_oh_one <owj0421@naver.com>
2024-12-28 14:31:59 +0900

    fix package import error

--- setup.py
+++ setup.py
@@ -1,55 +1,48 @@
 import setuptools
 from setuptools import setup
+from setuptools import setup, find_packages
+
+setup_requires = [
+]
+
+install_requires = [
+    "torch>=2.5.0",
+    "torchvision>=0.20.0",
+    "torchaudio>=2.5.0",
+    "pillow>=11.0.0",
+    "transformers>=4.46.1",
+    "wandb",
+    "tqdm",
+    "scikit-learn",
+    "gradio",
+    "faiss-gpu",
+],
 
 # long description을 README.md로 대체하기 위한 작업
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-
-	# module 이름
-    name='', 
+    version='0.0.16',
     
-    # version 명시
-    version='0.0.4',
-    
-    # package에 대한 짧은 description
-    description='',
-    
-    # package에 대한 자세한 description
-    # README.md로 대체한다
+    name='fashion_recommenders', 
+    description='An easy-to-use PyTorch-based package for fashion recommendation models',
     long_description=long_description,
     long_description_content_type="text/markdown",
     
-    # package 저자 이름
     author='Wonjun, Oh',
-    
-    # package 저자 이메일 
     author_email='owj0421@naver.com',
+    packages=find_packages(where='src'),
     
-    # package url (ex: github)
-    url='',
+    url='https://github.com/owj0421/fashion_recommenders',
     license='MIT',
-    packages=setuptools.find_packages(),
-    
-    # 파이썬 버전
-    python_requires='>=3.10',  
-    
-    install_requires = [
-        "torch>=2.5.0",
-        "torchvision>=0.20.0",
-        "torchaudio>=2.5.0",
-        "pillow>=11.0.0",
-        "transformers>=4.46.1",
-        "wandb",
-        "tqdm",
-        "scikit-learn",
-        "gradio",
-        "faiss-gpu",
-    ],
+
+    python_requires='>=3.10',
+    setup_requires=setup_requires,
+    install_requires = install_requires,
     classifiers = [
-                      "Programming Language :: Python :: 3",
-                      "License :: OSI Approved :: MIT License",
-                      "Operating System :: OS Independent"
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
     ]
 )
